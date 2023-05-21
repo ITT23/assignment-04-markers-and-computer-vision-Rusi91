13902b3970844c5432609fd75fd545e1b99f5d61
Ruslan Asabidi <ruslanasabidi@gmail.com>
2023-05-21 12:31:51 +0200

    prototyp

--- ar_game/opencv_pyglet.py
+++ ar_game/opencv_pyglet.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyglet
 from PIL import Image
 import sys
+import cv2.aruco as aruco
 
 video_id = 0
 
@@ -36,12 +37,32 @@ cap = cv2.VideoCapture(video_id)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 
+# Define the ArUco dictionary and parameters
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+aruco_params = aruco.DetectorParameters()
+
 window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 @window.event
 def on_draw():
     window.clear()
     ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect ArUco markers in the frame
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
+
+    if len(corners) > 0:
+       print(corners[0][0])
+
+    # Check if marker is detected
+    if ids is not None:
+        # Draw lines along the sides of the marker
+        aruco.drawDetectedMarkers(frame, corners)
+
+
     img = cv2glet(frame, 'BGR')
     img.blit(0, 0, 0)
 
